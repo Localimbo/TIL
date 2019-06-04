32fa521a7b72d2024fd876590fa5223bc570421a
Bella <mnbq77@gmail.com>
2019-06-04 17:37:01 +0900

    0604

---
+++ 4_django/MODEL_BASIC/board/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+# class 정의
+
+
+class Article(models.Model):
+    # id = Primary Key
+    title = models.CharField(max_length=200) # 최대로 들어 갈 수 있는 길이 지정 필수!
+    content = models.TextField()
+
+    def __str__(self):
+        return f'{self.id}: {self.title} - {self.content}'
+
+
---
+++ 4_django/MODEL_BASIC/model_basic/urls.py
@@ -0,0 +1,9 @@
+# master urls~~~
+
+from django.contrib import admin
+from django.urls import path, include   # include는 forward의 의미
+
+urlpatterns = [
+    path('admin/', admin.site.urls),     # 장고는 앞에 / 쓰지 않고 뒤에서만 사용
+    path('board/', include('board.urls')),
+]
